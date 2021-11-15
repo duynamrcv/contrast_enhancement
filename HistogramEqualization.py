4d9b16ed2a2cbdc7ce98abc6f3e11007b6fa9145
duynamrcv <duynam.robotics@gmail.com>
2021-11-15 16:06:24 +0700

    Add DEE and HEQ

--- HistogramEqualization.py
+++ HistogramEqualization.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
+import numba
 from skimage import exposure as ex
+from im2dhist import im2dhist, imhist
 
 class HE():
     def contrast_enhancement(self, img):
@@ -14,3 +16,39 @@ class HE():
         outImg[outImg>255] = 255
         outImg[outImg<0] = 0
         return outImg.astype(np.uint8)
+
+class HEQ():
+    def __init__(self, w_neighboring=6):
+        self.w_neighboring = w_neighboring
+
+    def contrast_enhancement(self, img):
+        image_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        image_v = image_hsv[:, :, 2].copy()
+        image_v_heq = self.im2dhisteq(image_v)
+
+        image_hsv[:, :, 2] = image_v_heq.copy()
+        result = cv2.cvtColor(image_hsv, cv2.COLOR_HSV2BGR)
+        return result
+
+    def im2dhisteq(self, image):
+        V = image.copy()
+        [h, w] = V.shape
+        V_hist = imhist(V)
+        H_in = im2dhist(V, w_neighboring=self.w_neighboring)
+        CDFx = np.cumsum(np.sum(H_in, axis=0)) # Kx1
+
+        # normalizes CDFx
+        CDFxn = (255*CDFx/CDFx[-1])
+
+        PDFxn = np.zeros_like(CDFxn)
+        PDFxn[0] = CDFxn[0]
+        PDFxn[1:] = np.diff(CDFxn)
+
+        X_transform = np.zeros((256))
+        X_transform[np.where(V_hist > 0)] = PDFxn.copy()
+        CDFxn_transform = np.cumsum(X_transform)
+
+        bins = np.arange(256)
+        image_equalized = np.floor(np.interp(V.flatten(), bins, CDFxn_transform).reshape(h, w)).astype(np.uint8)
+
+        return image_equalized
--- test_constrast_enhancement.py
+++ test_constrast_enhancement.py
@@ -6,44 +6,39 @@ import glob
 
 from ExposureFusion import EFF
 from DynamicHistogram import DHE, BPDHE
-
-from HistogramEqualization import HE
+from EntropyPreservingMapping import EPMP
+from AdaptiveGammaCorrection import AGC
+from HistogramEqualization import HE, HEQ
+from DifferentEffectiveEnhancement import DEE
 
 def main():
     import time
 
     # Load Methods
-    eff = EFF()
+    # eff = EFF()   
     dhe = DHE()
+    epm = EPMP()
+    agc = AGC()
+    heq = HEQ()
+    dee = DEE()
 
     st = time.time()
     # img_name = sys.argv[1]
-    # img_name = "images/test1/H30_1320_.JPG"
-    paths = glob.glob("images/test1/*.JPG")
-    for i in range (10):
-        name = paths[i].split("\\")[-1].split('.')[0]
-        print(name)
-        img = cv2.imread(paths[i])
-        img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
-        result_eff = eff.contrast_enhancement(img)
-        result_dhe = dhe.contrast_enhancement(img)
-        en = time.time()
-        print("Process time: {}s".format(en-st))
+    img_name = "images/test1/H30_0074_.JPG"
+    img = cv2.imread(img_name)
+    result = dee.contrast_enhancement(img)
+    en = time.time()
+    print("Process time: {}s".format(en-st))
 
-        # Save images
-        cv2.imwrite("results/"+name+".JPG", img)
-        cv2.imwrite("results/"+name+"_eff.JPG", result_eff)
-        cv2.imwrite("results/"+name+"_dhe.JPG", result_dhe)
-
-        # Show results
-        # plt.figure()
-        # plt.subplot(131)
-        # plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-        # plt.subplot(132)
-        # plt.imshow(cv2.cvtColor(result_eff, cv2.COLOR_BGR2RGB))
-        # plt.subplot(133)
-        # plt.imshow(cv2.cvtColor(result_dhe, cv2.COLOR_BGR2RGB))
-        # plt.show()
+    # Show results
+    plt.figure()
+    plt.subplot(121)
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    plt.subplot(122)
+    plt.imshow(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
+    # plt.subplot(133)
+    # plt.imshow(cv2.cvtColor(result_dhe, cv2.COLOR_BGR2RGB))
+    plt.show()
 
 if __name__ == '__main__':
     main()
