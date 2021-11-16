bfb0475902913ccbfad45ded9245bddf5db35b5a
duynamrcv <duynam.robotics@gmail.com>
2021-11-16 15:04:42 +0700

    Update code

--- test_constrast_enhancement.py
+++ test_constrast_enhancement.py
@@ -15,30 +15,48 @@ def main():
     import time
 
     # Load Methods
-    # eff = EFF()   
+    eff = EFF()   
     dhe = DHE()
-    epm = EPMP()
-    agc = AGC()
-    heq = HEQ()
-    dee = DEE()
+    # epm = EPMP()
+    # agc = AGC()
+    # heq = HEQ()
+    # dee = DEE()
 
+    # paths = glob.glob("images/test1/*.JPG")
+    # paths = glob.glob("test1/*")
+    # for img_name in paths:
     st = time.time()
+    img_name = "test1\\01.jfif"
+    name = img_name.split('\\')[-1].split('.')[0]
     # img_name = sys.argv[1]
-    img_name = "images/test1/H30_0074_.JPG"
+    # img_name = "images/test1/H30_0074_.JPG"
     img = cv2.imread(img_name)
-    result = dee.contrast_enhancement(img)
+    res_eff = eff.contrast_enhancement(img)
+    res_dhe = dhe.contrast_enhancement(img)
+    # res_epm = epm.contrast_enhancement(img)
+    # res_agc = agc.contrast_enhancement(img)
+    # res_heq = heq.contrast_enhancement(img)
+    # res_dee = dee.contrast_enhancement(img)
+    # result = dee.contrast_enhancement(img)
+
+    cv2.imwrite("results/{}_eff.jpg".format(name), res_eff)
+    cv2.imwrite("results/{}_dhe.jpg".format(name), res_dhe)
+        # cv2.imwrite("results/{}_epm.jpg".format(name), res_epm)
+        # cv2.imwrite("results/{}_agc.jpg".format(name), res_agc)
+        # cv2.imwrite("results/{}_heq.jpg".format(name), res_heq)
+        # cv2.imwrite("results/{}_dee.jpg".format(name), res_dee)
     en = time.time()
     print("Process time: {}s".format(en-st))
 
     # Show results
-    plt.figure()
-    plt.subplot(121)
-    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    plt.subplot(122)
-    plt.imshow(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
+    # plt.figure()
+    # plt.subplot(121)
+    # plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    # plt.subplot(122)
+    # plt.imshow(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
     # plt.subplot(133)
     # plt.imshow(cv2.cvtColor(result_dhe, cv2.COLOR_BGR2RGB))
-    plt.show()
+    # plt.show()
 
 if __name__ == '__main__':
     main()
